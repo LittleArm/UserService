f22b2bf738bc181d6aa9a08fe7dc83c546354a2e
LittleArm <hien.lequang47@hcmut.edu.vn>
2024-07-09 16:11:39 +0700

    Add RoleRepo precheck

--- src/main/java/com/example/userservice/UserserviceApplication.java
+++ src/main/java/com/example/userservice/UserserviceApplication.java
@@ -1,13 +1,36 @@
 package com.example.userservice;
 
+import com.example.userservice.model.Role;
+import com.example.userservice.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+			if (roleRepository.findByName("ADMIN").isEmpty()) {
+				roleRepository.save(
+						Role.builder().name("ADMIN").build()
+				);
+			}
+		};
+	}
 }
